7872c9ba49c8c9fba61db6d68ff8516486bc8aa5
akkassingh <akkassingh@gmail.com>
2024-12-08 15:52:31 -0800

    initial commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen flex flex-col items-center justify-center bg-gray-100 dark:bg-gray-900 text-gray-900 dark:text-gray-100 p-4`}
       >
         {children}
       </body>
